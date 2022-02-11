0cf57e69a4743c7c61a37466e0cc1ff16e61d008
Bradders81 <p-bradley@live.co.uk>
2022-02-11 11:53:48 +0000

    Add prodcut info view/template  & search query to prodcuts view

--- products/views.py
+++ products/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.db.models import Q
+from django.contrib import messages
 from .models import Wine
+
 # Create your views here.
 
 
 def products(request):
     """
-    Returns the Prodcuts  page
+    Returns the Prodcuts page and handles
+    search queries for products
     """
-    wines = Wine.objects.all()
     
-    context = {
-        'wines': wines,
-    }
+    wines = Wine.objects.all()
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+        wines = Wine.objects.filter(
+            Q(name__icontains=search_query) |
+            Q(type__icontains=search_query) |
+            Q(country__icontains=search_query) |
+            Q(year__icontains=search_query))
+
+        if not wines:
+            messages.error(request, "Nothing matches your search")
+
+            
+
+    context = {'wines': wines, }
     return render(request, 'products/products.html', context)
 
 
@@ -22,10 +38,7 @@ def product_info(request, pk):
     wines = Wine.objects.all()
     bottle = Wine.objects.get(id=pk)
 
-    context = {
-        'wines': wines,
-        'bottle': bottle,
-    }
+    context = {'wines': wines, 'bottle': bottle, }
 
     return render(request, 'products/product-info.html', context)
 
