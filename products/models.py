77c04af7ccae7a880190c3babe5eca54f107504c
Bradders81 <p-bradley@live.co.uk>
2022-02-13 23:17:43 +0000

    Add choices to wine_type in Wine class models.py

--- products/models.py
+++ products/models.py
@@ -6,12 +6,23 @@ import uuid
 # Create your models here.
 
 
+
 class Wine(models.Model):
     """
-    Model of the wine in the database
+    Model of the wines in the database
     """
     class Meta:
         verbose_name_plural = 'Wine'
+
+    Red = 'Red'
+    White = 'White'
+    Rosé = 'Rosé'
+
+    WINE_TYPES = [
+        (Red, Red),
+        (White, White),
+        (Rosé, Rosé),
+    ]
 
     wine_name = models.CharField(max_length=100)
     image = models.ImageField(null=True, blank=True, upload_to='images/')
@@ -19,7 +30,7 @@ class Wine(models.Model):
     winery = models.CharField(max_length=50, null=True, blank=True)
     # rating = models.ManyToManyField(Reviews, models.SET_NULL, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    wine_type = models.CharField(max_length=50)
+    wine_type = models.CharField(choices=WINE_TYPES, max_length=5)
     description = models.TextField(null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
     id = models.UUIDField(
