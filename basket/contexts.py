dc4b604df3271d1ae2a9a755e9165b7e36ad47b7
Bradders81 <p-bradley@live.co.uk>
2022-02-24 10:30:24 +0000

    Add basket content logic

---
+++ basket/contexts.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+
+def basket_contents(request):
+    """
+    Handles the basket total on the basket page
+    """
+    basket_items = []
+    total = 0
+    product_count = 0
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count
+    }
+
+    return context
+
