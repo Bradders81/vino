5e3df6ccd478bea4925c5ad05a0d683b0beb12eb
Bradders81 <p-bradley@live.co.uk>
2022-03-01 14:39:46 +0000

    Fix typo

--- vino/settings.py
+++ vino/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'vino.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DARABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
