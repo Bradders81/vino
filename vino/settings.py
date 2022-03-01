0269ecb80c0e76af1fb0cac927922eab9dbf335a
Bradders81 <p-bradley@live.co.uk>
2022-03-01 12:24:40 +0000

    Deploys static AWS to Heroku

--- vino/settings.py
+++ vino/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'products',
     'profiles',
     'basket',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -139,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalisation
+# Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
@@ -161,10 +162,25 @@ MEDIA_URL = '/media/images/'
 
 STATICFILES_DIRS = [BASE_DIR/'static/']
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 
+if  'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'bradders81-vino'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
