7cfdc348890fdb522f8fb4cf4b3fa239059b3647
Bradders81 <p-bradley@live.co.uk>
2022-03-01 17:54:16 +0000

    Add basket logic and basic template layout

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Wine
 
 def basket_contents(request):
     """
@@ -7,6 +9,20 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        wine = get_object_or_404(Wine, pk=item_id)
+        total += quantity * wine.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'wine': wine,
+
+            
+        })
+    
 
     context = {
         'basket_items': basket_items,
--- basket/views.py
+++ basket/views.py
@@ -17,11 +17,10 @@ def add_to_basket(request, pk):
     """
     Add a item and quantify to the shopping basket
     """
-    # bottle = Wine.objects.get(id=pk)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
-    print(quantity)
+    
 
 
     if pk in list(basket.keys()):
@@ -31,7 +30,7 @@ def add_to_basket(request, pk):
         basket[pk] = quantity
 
     request.session['basket'] = basket
-    print(request.session['basket'])
+    
 
     return redirect(redirect_url)
 
