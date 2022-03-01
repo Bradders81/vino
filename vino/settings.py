227bc3d29b428743ea28b655273dc12e350e97b2
Bradders81 <p-bradley@live.co.uk>
2022-03-01 13:01:56 +0000

    Add cache control

--- vino/settings.py
+++ vino/settings.py
@@ -165,6 +165,12 @@ STATICFILES_DIRS = [BASE_DIR/'static/']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 
 if  'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'bradders81-vino'
     AWS_S3_REGION_NAME = 'eu-west-2'
