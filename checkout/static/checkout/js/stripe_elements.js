0e15c2d216525ec4f5a141f19d79197915f4e689
Bradders81 <p-bradley@live.co.uk>
2022-03-03 03:30:47 +0000

    Add simple checkout logic

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,16 +1,8 @@
-/*
-    Core logic/payment flow for this comes from here:
-    https://stripe.com/docs/payments/accept-a-payment
-
-    CSS from here: 
-    https://stripe.com/docs/stripe-js
-*/
-
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements();
-var card = elements.create('card', {style: style});
+let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+let client_secret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripe_public_key);
+let elements = stripe.elements();
+let card = elements.create('card', {style: style});
 var style = {
     base: {
         color: '#000',
@@ -28,3 +20,47 @@ var style = {
 };
 
 card.mount('#card-element');
+
+//Handle validation errors on the card element
+card.addEventListener('change', function (event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert"></span>
+            <spain>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = ''
+    }
+    
+});
+
+// Handle form submit
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert"></span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+                
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,40 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
 from django.contrib import messages
 from .forms import OrderForm
+from basket.contexts import basket_contents
+import stripe
 
 
 def checkout(request):
     """
-    Handles check logic
+    Handles checkout logic and stripe
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your bag is empty")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KYiy4CYO85OqnEi7NcYXGVkYvuKNkSBlRGlGW4KFBqRxB7as8x6yNaEUDNpaAayStwKZxTQXcSD4KtxRynDT9ni00lTYKLYf0',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+        
      }
 
     return render(request, template, context,)
