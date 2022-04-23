5444a6461f315a15e5538293afd26dca0b345f58
Bradders81 <p-bradley@live.co.uk>
2022-04-23 14:46:37 +0000

    Add ACCOUNT_EMAIL_VERIFICATION = 'none' to setting.py

--- vino/settings.py
+++ vino/settings.py
@@ -207,6 +207,8 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'vino@example.com'
