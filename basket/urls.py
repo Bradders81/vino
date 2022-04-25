435e65f6da366509d8407546d41f095122098e0e
Bradders81 <p-bradley@live.co.uk>
2022-04-25 07:26:52 +0000

    Add view to edit item qty in basket

--- basket/urls.py
+++ basket/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('basket/', views.view_basket, name='basket'),
     path('add_to_basket/<str:pk>', views.add_to_basket, name='add_to_basket'),
+    path('change_quantity/<str:pk>', views.change_quantity,
+         name='change_quantity'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -13,9 +13,9 @@ def view_basket(request):
 
 
 def add_to_basket(request, pk):
-
     """
     Add a item and quantify to the shopping basket
+    from the product-info page
     """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -29,7 +29,36 @@ def add_to_basket(request, pk):
         basket[pk] = quantity
 
     request.session['basket'] = basket
-    
+    # This cookie is needed for the change quantity view below
+    request.session['quantity'] = quantity
+
     return redirect(redirect_url)
 
+
+def change_quantity(request, pk):
+    """
+    View to allow users to edit the quantity of
+    items in the basket, up to a maximum of 50 per item
+    """
+    new_quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket')
+    current_quantity = request.session.get('quantity')
+
+    if new_quantity > current_quantity:
+        updated_quantity = int(new_quantity - current_quantity)
+
+        if updated_quantity + current_quantity <= 50:
+            basket[pk] += updated_quantity
+        else:
+            updated_quantity = 50
+            basket[pk] += updated_quantity
+    else:
+        updated_quantity = int(current_quantity - new_quantity)
+        basket[pk] -= updated_quantity
+
+    request.session['basket'] = basket
+    request.session['quantity'] = new_quantity
+
+    return redirect(redirect_url)
 
