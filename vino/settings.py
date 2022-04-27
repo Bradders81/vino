e5092b2c614ef8e97c5059a8de2e65fca78dc371
Bradders81 <p-bradley@live.co.uk>
2022-04-27 18:06:31 +0000

    Create review app file strucutre

--- vino/settings.py
+++ vino/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'storages',
     'checkout',
     'profiles1',
+    'reviews',
  
 ]
 
--- vino/urls.py
+++ vino/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path('profile/', include('profiles1.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
