5ccf2ae9cad06fea4bf1d1f4e81038eed5f5e60b
Bradders81 <p-bradley@live.co.uk>
2022-05-02 05:57:44 +0000

    Add forms.py to products app

---
+++ products/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Wine
+
+class ProductForm(forms.ModelForm):
+
+    """
+    Form for superuser/site owner to 
+    update products.
+    |"""
+
+    class Meta:
+        model = Wine
+        fields = '__all__'
+
