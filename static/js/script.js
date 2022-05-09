e559e4971d48ff40ce8e7e53549dd68e53da5e9c
Bradders81 <p-bradley@live.co.uk>
2022-05-09 06:34:07 +0000

    Add Javascript to make cta-btn flash

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+
+    function callToActionWarning() {
+        let ctaBtn = document.getElementById("btn-cta");
+        ctaBtn.classList.remove("btn-danger");
+        ctaBtn.classList.add("btn-warning");
+    };
+
+    function callToActionDanger() {
+        let ctaBtn = document.getElementById("btn-cta");
+        ctaBtn.classList.remove("btn-warning");
+        ctaBtn.classList.add("btn-danger");
+    }
+
+    setInterval(callToActionWarning, 1000);
+
+    setInterval(callToActionDanger, 2000);
+
+});
